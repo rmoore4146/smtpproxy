d5ea20be1b3c78304a64368f98413d0dd4a5a836
Ryan Moore <ryan.moore@avoxi.com>
2017-04-27 11:15:51 -0400

    switching to hostname

--- smtpproxy.go
+++ smtpproxy.go
@@ -45,9 +45,9 @@ func listen() (net.Listener, error) {
 func handleConnection(conn smtpd.Connection) {
 	defer conn.Close()
 	fmt.Println(argerror.New("New connection",
-		map[string]string{"client": conn.RemoteAddr().String()}))
+		map[string]string{"client": hostname()}))
 	defer fmt.Println(argerror.New("Connection finished",
-		map[string]string{"client": conn.RemoteAddr().String()}))
+		map[string]string{"client": hostname()}))
 	state, err := proxy.Greet(conn)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -67,7 +67,7 @@ func maybeTarpit(err error, conn smtpd.Connection) {
 	_, ok := err.(proxy.TarpitError)
 	if ok {
 		args := map[string]string{
-			"client": conn.RemoteAddr().String(),
+			"client": hostname(),
 		}
 		bytesread, duration, err := conn.Tarpit()
 		args["bytesread"] = fmt.Sprintf("%d", bytesread)
@@ -75,4 +75,12 @@ func maybeTarpit(err error, conn smtpd.Connection) {
 		args["error"] = err.Error()
 		fmt.Println(argerror.New("Client escaped tarpit", args))
 	}
+}
+
+func hostname() string {
+	name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return name
 }
